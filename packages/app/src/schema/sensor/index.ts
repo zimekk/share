6c2dae5f9cd7ec395b7c41cfd5271e6bad1506bf
Marcin Makarewicz <marcin@makarewicz.eu>
2022-08-15 16:59:13 +0200

    ci(app): fix tests

--- packages/app/src/schema/sensor/index.ts
+++ packages/app/src/schema/sensor/index.ts
@@ -1,6 +1,9 @@
 import gql from "graphql-tag";
 import { makeExecutableSchema } from "@graphql-tools/schema";
+/*
 import noble from "@abandonware/noble";
+
+var async = require('async');
 
 noble.on("stateChange", async (state) => {
   console.log({ state });
@@ -10,14 +13,187 @@ noble.on("stateChange", async (state) => {
     // await noble.startScanningAsync([], false);
     // await noble.startScanningAsync(['180f'], false);
     await noble.startScanningAsync(
-      ["00010203-0405-0607-0809-0a0b0c0d1912"],
-      false
+      ["fe95"],
+      true
     );
     // await noble.startScanningAsync();
   }
 });
 
-noble.on("discover", async (peripheral) => {
+noble.on('discover', function(peripheral) {
+  // if (peripheral.id === peripheralIdOrAddress || peripheral.address === peripheralIdOrAddress) {
+  //   noble.stopScanning();
+
+    console.log('peripheral with ID ' + peripheral.id + ' found');
+    var advertisement = peripheral.advertisement;
+
+    var localName = advertisement.localName;
+    var txPowerLevel = advertisement.txPowerLevel;
+    var manufacturerData = advertisement.manufacturerData;
+    var serviceData = advertisement.serviceData;
+    var serviceUuids = advertisement.serviceUuids;
+
+    if (localName) {
+      console.log('  Local Name        = ' + localName);
+    }
+
+    if (txPowerLevel) {
+      console.log('  TX Power Level    = ' + txPowerLevel);
+    }
+
+    if (manufacturerData) {
+      console.log('  Manufacturer Data = ' + manufacturerData.toString('hex'));
+    }
+
+    if (serviceData) {
+      console.log('  Service Data      = ' + JSON.stringify(serviceData, null, 2));
+      console.log(serviceData[0].data)
+    }
+
+    if (serviceUuids) {
+      console.log('  Service UUIDs     = ' + serviceUuids);
+    }
+
+    // console.log();
+
+    explore(peripheral);
+  // }
+});
+
+function explore(peripheral) {
+  console.log('services and characteristics:');
+
+  // peripheral.on('disconnect', function() {
+  //   process.exit(0);
+  // });
+
+  peripheral.connect(function(error) {
+    peripheral.discoverServices([], function(error, services) {
+      var serviceIndex = 0;
+
+      async.whilst(
+        function () {
+          return (serviceIndex < services.length);
+        },
+        function(callback) {
+          var service = services[serviceIndex];
+          var serviceInfo = service.uuid;
+
+          if (service.name) {
+            serviceInfo += ' (' + service.name + ')';
+          }
+          console.log(serviceInfo);
+
+          service.discoverCharacteristics([], function(error, characteristics) {
+            var characteristicIndex = 0;
+
+            async.whilst(
+              function () {
+                return (characteristicIndex < characteristics.length);
+              },
+              function(callback) {
+                var characteristic = characteristics[characteristicIndex];
+                var characteristicInfo = '  ' + characteristic.uuid;
+
+                if (characteristic.name) {
+                  characteristicInfo += ' (' + characteristic.name + ')';
+                }
+
+                async.series([
+                  function(callback) {
+                    characteristic.discoverDescriptors(function(error, descriptors) {
+                      async.detect(
+                        descriptors,
+                        function(descriptor, callback) {
+                          if (descriptor.uuid === '2901') {
+                            return callback(descriptor);
+                          } else {
+                            return callback();
+                          }
+                        },
+                        function(userDescriptionDescriptor){
+                          if (userDescriptionDescriptor) {
+                            userDescriptionDescriptor.readValue(function(error, data) {
+                              if (data) {
+                                characteristicInfo += ' (' + data.toString() + ')';
+                              }
+                              callback();
+                            });
+                          } else {
+                            callback();
+                          }
+                        }
+                      );
+                    });
+                  },
+                  function(callback) {
+                        characteristicInfo += '\n    properties  ' + characteristic.properties.join(', ');
+
+                    if (characteristic.properties.indexOf('read') !== -1) {
+                      characteristic.read(function(error, data) {
+                        if (data) {
+                          var string = data.toString('ascii');
+
+                          characteristicInfo += '\n    value       ' + data.toString('hex') + ' | \'' + string + '\'';
+                        }
+                        callback();
+                      });
+                    } else {
+                      callback();
+                    }
+                  },
+                  function() {
+                    console.log(characteristicInfo);
+                    characteristicIndex++;
+                    callback();
+                  }
+                ]);
+              },
+              function(error) {
+                serviceIndex++;
+                callback();
+              }
+            );
+          });
+        },
+        function (err) {
+          peripheral.disconnect();
+        }
+      );
+    });
+  });
+}
+
+noble.on('discover-', function(peripheral) {
+  console.log('peripheral discovered (' + peripheral.id +
+              ' with address <' + peripheral.address +  ', ' + peripheral.addressType + '>,' +
+              ' connectable ' + peripheral.connectable + ',' +
+              ' RSSI ' + peripheral.rssi + ':');
+  console.log('\thello my local name is:');
+  console.log('\t\t' + peripheral.advertisement.localName);
+  console.log('\tcan I interest you in any of the following advertised services:');
+  console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
+
+  var serviceData = peripheral.advertisement.serviceData;
+  if (serviceData && serviceData.length) {
+    console.log('\there is my service data:');
+    for (var i in serviceData) {
+      console.log('\t\t' + JSON.stringify(serviceData[i].uuid) + ': ' + JSON.stringify(serviceData[i].data.toString('hex')));
+    }
+  }
+  if (peripheral.advertisement.manufacturerData) {
+    console.log('\there is my manufacturer data:');
+    console.log('\t\t' + JSON.stringify(peripheral.advertisement.manufacturerData.toString('hex')));
+  }
+  if (peripheral.advertisement.txPowerLevel !== undefined) {
+    console.log('\tmy TX power level is:');
+    console.log('\t\t' + peripheral.advertisement.txPowerLevel);
+  }
+
+  console.log();
+});
+
+noble.on("discover-", async (peripheral) => {
   console.log(peripheral);
   console.log(peripheral.advertisement?.manufacturerData?.toString("hex"));
   console.log(peripheral.advertisement?.serviceData);
@@ -53,6 +229,7 @@ noble.on("discover", async (peripheral) => {
 // }).catch((err) => {
 //     console.error(`[xiaomi-mijia-lywsd03mmc] Unable to get sensors (error: ${err})`)
 // })
+*/
 
 export default makeExecutableSchema({
   typeDefs: gql`
--- packages/web/src/containers/Sensor/index.tsx
+++ packages/web/src/containers/Sensor/index.tsx
@@ -109,6 +109,7 @@ function miAction() {
           "characteristicvaluechanged",
           (event) => {
             var value = event.target.value;
+            console.log({ value });
             var sign = value.getUint8(1) & (1 << 7);
             var temp = ((value.getUint8(1) & 0x7f) << 8) | value.getUint8(0);
             if (sign) temp = temp - 32767;
@@ -206,14 +207,29 @@ export default function Sensor() {
 
   const onConnect = useCallback<MouseEventHandler<HTMLButtonElement>>(
     (event) => {
+      // https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth/requestDevice
       const deviceOptions = {
-        filters: [
-          {
-            services: [0xfe95],
-          },
+        // filters: [
+        //   { name: "LYWSD03MMC" },
+        //   {
+        //     services: [0xfe95],
+        //   },
+        //   {
+        //     services: [
+        //       "00010203-0405-0607-0809-0a0b0c0d1912",
+        //       "ebe0ccb0-7a0a-4b0c-8a1a-6ff2997da3a6",
+        //       0xfe95,
+        //       0x1f10,
+        //     ],
+        //   },
+        // ],
+        optionalServices: [
+          "00010203-0405-0607-0809-0a0b0c0d1912",
+          "ebe0ccb0-7a0a-4b0c-8a1a-6ff2997da3a6",
+          0xfe95,
+          0x1f10,
         ],
-        // optionalServices: ['00010203-0405-0607-0809-0a0b0c0d1912', 'ebe0ccb0-7a0a-4b0c-8a1a-6ff2997da3a6', 0xfe95, 0x1f10],
-        // acceptAllDevices: true,
+        acceptAllDevices: true,
       };
       navigator.bluetooth.getAvailability().then((availability) => {
         if (availability) {
