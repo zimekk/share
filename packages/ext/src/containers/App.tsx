87d05d2222b21dd1c4a545d818a74980e18c3651
Marcin Makarewicz <marcin@makarewicz.eu>
2023-04-01 22:58:29 +0200

    feat(ext): chrome extension

---
+++ packages/ext/src/containers/App.tsx
@@ -0,0 +1,46 @@
+import React, { Suspense, lazy, useEffect, useState } from "react";
+import { hot } from "react-hot-loader/root";
+import history from "history/browser";
+import styles from "./App.module.scss";
+
+const Spinner = () => <span>Loading...</span>;
+
+const PAGES = {
+  photos: lazy(() => import("@dev/photos")),
+};
+
+const getPage = (location: { hash: string }) => {
+  const [_, hash] = decodeURI(location.hash).match(/^#([-\w]+)/) || [];
+  return ((keys) => (keys.includes(hash) ? hash : keys[0]))(
+    Object.keys(PAGES)
+  ) as keyof typeof PAGES;
+};
+
+function App() {
+  const [page, setPage] = useState(getPage(history.location));
+
+  useEffect(() =>
+    // location is an object like window.location
+    history.listen(({ location }) => setPage(getPage(location)))
+  );
+
+  const Page = PAGES[page];
+
+  return (
+    <section className={styles.App}>
+      <h1 className={styles.Nav}>
+        Share
+        {Object.keys(PAGES).map((page) => (
+          <a key={page} href={`#${page}`}>
+            {page}
+          </a>
+        ))}
+      </h1>
+      <Suspense fallback={<Spinner />}>
+        <Page />
+      </Suspense>
+    </section>
+  );
+}
+
+export default hot(App);
---
+++ packages/ext/webpack.config.ts
@@ -0,0 +1,107 @@
+import path from "path";
+import webpack from "webpack";
+import env from "dotenv";
+
+const { MI_USERNAME = "", MI_PASSWORD = "" } = env.config({
+  path: path.resolve(__dirname, "../../.env"),
+}) as Record<string, string>;
+
+export default (env, { mode }, dev = mode === "development") => ({
+  target: "web",
+  devtool: dev ? "eval-cheap-source-map" : "source-map",
+  entry: {
+    main: [
+      // "webpack-dev-server/client?http://localhost:8080/test",
+      "react-hot-loader/patch",
+    ].concat(require.resolve("./src")),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|mov|mp3|ogg|png|avi)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react", "@babel/preset-typescript"],
+          plugins: [].concat(dev ? "react-hot-loader/babel" : []),
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      events: "events",
+      "react-dom": "@hot-loader/react-dom",
+    },
+    fallback: {
+      buffer: require.resolve("buffer"),
+      stream: require.resolve("stream-browserify"),
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    clean: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.EnvironmentPlugin({
+      FFMPEG_CORE_PATH: "ffmpeg/ffmpeg-core.js",
+      MI_USERNAME,
+      MI_PASSWORD,
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new (require("copy-webpack-plugin"))({
+      patterns: [
+        {
+          context: path.resolve(__dirname, "src/assets"),
+          from: "manifest.json",
+          // to: "[name]",
+          transform: function (content, path) {
+            // generates the manifest file using the package.json informations
+            return Buffer.from(
+              JSON.stringify(
+                {
+                  // description: process.env.npm_package_description,
+                  version: process.env.npm_package_version,
+                  ...JSON.parse(content.toString()),
+                },
+                null,
+                2
+              )
+            );
+          },
+        },
+      ],
+    }),
+    // https://webpack.js.org/plugins/html-webpack-plugin/
+    new (require("html-webpack-plugin"))({
+      favicon: require.resolve("./src/assets/favicon.ico"),
+      // meta: {
+      //   referrer: 'no-referrer'
+      // }
+    }),
+  ].filter(Boolean),
+});
